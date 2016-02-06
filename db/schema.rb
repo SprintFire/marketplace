f43b844ff6ccec0375702ac4cd2e73cd12ad7068
Levin George <levingeorge41@gmail.com>
2016-02-06 19:19:36 +0000

    added migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206184054) do
+ActiveRecord::Schema.define(version: 20160206190813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20160206184054) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id",     null: false
   end
+
+  add_index "products", ["user_id"], name: "index_products_on_user_id", using: :btree
 
   create_table "shops", force: :cascade do |t|
     t.string   "name"
@@ -30,9 +33,11 @@ ActiveRecord::Schema.define(version: 20160206184054) do
     t.string   "slug"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id",     null: false
   end
 
   add_index "shops", ["slug"], name: "index_shops_on_slug", using: :btree
+  add_index "shops", ["user_id"], name: "index_shops_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
