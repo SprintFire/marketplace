a3477eb0e74a2bee03896f784f1c08914e984632
Levin George <levingeorge41@gmail.com>
2016-02-19 22:54:17 +0000

    updated the indentation

--- db/migrate/20160206184054_create_products.rb
+++ db/migrate/20160206184054_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
-	def change
-		create_table :products do |t|
-  		t.string :name
-  		t.text :description
-  		t.numeric :price 
-  		t.timestamps null: false
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.numeric :price 
+      t.timestamps null: false
     end
-	end
+  end
 end
