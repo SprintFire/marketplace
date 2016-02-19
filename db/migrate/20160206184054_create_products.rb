2d66509ab2f3479b2d6de67987601d8522e5fab0
Levin George <levingeorge41@gmail.com>
2016-02-19 22:29:58 +0000

    uploading a create_products migration file

--- db/migrate/20160206184054_create_products.rb
+++ db/migrate/20160206184054_create_products.rb
@@ -1,9 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
 	def change
 		create_table :products do |t|
-		t.string :name
-		t.text :description
-		t.numeric :price 
-		t.timestamps null: false
+  		t.string :name
+  		t.text :description
+  		t.numeric :price 
+  		t.timestamps null: false
+    end
 	end
 end
