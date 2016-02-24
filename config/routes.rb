6da7542c695d13d76072355709bc69cabe25fc20
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-02-24 15:38:24 +0000

    Fix bug of comma.

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  get "dashboard" => "pages#dashboard"， as: "dashboard"
+  get "dashboard" => "pages#dashboard", as: "dashboard"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
