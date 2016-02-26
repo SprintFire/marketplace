62f167a8eb8cee9f506932011d76703fa8c6a0b0
Levin George <levingeorge41@gmail.com>
2016-02-26 12:52:21 +0000

    updating google auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, :omniauth_providers => [:facebook,:google_oauth2]
   has_many :shops
 
   def self.from_omniauth(auth)
@@ -20,6 +20,20 @@ class User < ActiveRecord::Base
       end
     end
   end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    # unless user
+    #     user = User.create(name: data["name"],
+    #        email: data["email"],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
+    user
+end
 
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,8 +20,11 @@ Devise.setup do |config|
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
 
-  # ==> Mailer Configuration
+  # ==> Facebook Authentication
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"]
+  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],{
+    :scope => "email,profile",:access_type => "online"
+  }
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
