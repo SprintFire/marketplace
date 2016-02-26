c88366c6cb6f0aac2ccd2835bb07d7e32d4f0421
Levin George <levingeorge41@gmail.com>
2016-02-26 13:29:15 +0000

    updating google auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -25,20 +25,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
      end
   end
 
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(:email => data["email"]).first
-
-    # Uncomment the section below if you want users to be created if they don't exist
-    # unless user
-    #     user = User.create(name: data["name"],
-    #        email: data["email"],
-    #        password: Devise.friendly_token[0,20]
-    #     )
-    # end
-    user
-end
-
   def failure
     redirect_to root_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,19 @@ class User < ActiveRecord::Base
     end
   end
 
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
 
+    # Uncomment the section below if you want users to be created if they don't exist
+    # unless user
+    #     user = User.create(name: data["name"],
+    #        email: data["email"],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
+    user
+end
 
 
 end
