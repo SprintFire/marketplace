8193274e0402924bf04d686f06825d18dbfbc502
danielpaul <danielpaul2008@gmail.com>
2016-02-26 22:06:12 +0000

    Updated to OmniauthCallbacksController

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,10 +4,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Facebook'
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
+      flash[:notice] = 'Looks like you already have an account with the same email. Please login using a different method first to connect with Facebook.'
       redirect_to new_user_registration_url
     end
   end
@@ -17,10 +19,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
      @user = User.from_omniauth(request.env["omniauth.auth"])
 
      if @user.persisted?
-       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-       sign_in_and_redirect @user, :event => :authentication
+       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+       sign_in_and_redirect @user, event: :authentication
      else
        session["devise.google_data"] = request.env["omniauth.auth"]
+       flash[:notice] = 'Looks like you already have an account with the same email. Please login using a different method first to connect with Google.'
        redirect_to new_user_registration_url
      end
   end
