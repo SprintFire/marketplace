c391f399fafc2c2e6c9c01df71b690fcb0a13bd6
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-02-26 12:23:48 +0000

    Add index.

--- db/migrate/20160226103946_add_location_to_shop.rb
+++ db/migrate/20160226103946_add_location_to_shop.rb
@@ -2,5 +2,6 @@ class AddLocationToShop < ActiveRecord::Migration
   def change
     add_column :shops, :longitude, :numeric
     add_column :shops, :latitude, :numeric
+    add_index  :shops, [:lat, :lng]
   end
 end
