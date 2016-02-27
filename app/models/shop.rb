624fd182e80b62748d830ab7313d575f12f4ce29
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-02-27 16:08:45 +0000

    Remove geocoder for now, fix bug.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,6 +2,6 @@ class Shop < ActiveRecord::Base
 	belongs_to :user
 	has_many :products, :dependent => :delete_all
 
-	geocoded_by :full_street_address
-	after_validation :geocode
+	#geocoded_by :full_street_address
+	#after_validation :geocode
 end
