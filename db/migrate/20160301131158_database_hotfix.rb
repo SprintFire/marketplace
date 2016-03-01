00691fae7962ca8ef35cd546b833312912dcacf4
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-03-01 14:36:36 +0000

    Lorem ipsum...

--- db/migrate/20160301131158_database_hotfix.rb
+++ db/migrate/20160301131158_database_hotfix.rb
@@ -1,6 +1,7 @@
 class DatabaseHotfix < ActiveRecord::Migration
   def change
-    change_column :products, :quantity, :integer # Need to validate as positive interger
+    change_column :products, :quantity, :integer
+    # Need to validate as positive interger
 
     change_column :shops, :contact_phone, "char(20)"
 
