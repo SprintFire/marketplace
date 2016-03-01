3b0a7fb0ffff838231fb8aeb4bdff9e0d3f340dc
Ionatan Sala <sala.jonatan1995@gmail.com>
2016-03-01 13:42:09 +0000

    use fog gem for poduction in the user_profile_uploader

--- app/uploaders/user_profile_uploader.rb
+++ app/uploaders/user_profile_uploader.rb
@@ -9,8 +9,11 @@ class UserProfileUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [168, 168]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
