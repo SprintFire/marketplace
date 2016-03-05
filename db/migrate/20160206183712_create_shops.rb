0a7027886ea7c94438cc81c291791163a91b14c2
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-03-05 15:39:09 +0000

    Fix merge conflicts.

--- db/migrate/20160206183712_create_shops.rb
+++ db/migrate/20160206183712_create_shops.rb
@@ -3,7 +3,7 @@ class CreateShops < ActiveRecord::Migration
     create_table :shops do |t|
       t.string :name
       t.text :description
-      t.string :slug, :unique=> true, :index => true
+      t.string :slug, :unique => true, :index => true
       t.timestamps null: false
     end
   end
