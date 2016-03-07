8b76978da4b80a3efa40f8952e67eb1f7f97e7b5
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-03-07 14:21:25 +0000

    Add Recent Items to the home page of Active Admin Dashboard.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,10 +3,59 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Admin Notes" do
+          para "Welcome to the Marketplace Admin page. Here you can find some useful statistic intelligence of the Marketplace."
+        end
+      end
+    end
+
+    columns do
+      column do
+        panel "Recent Registered Users" do
+          table_for User.order('id desc').limit(10).each do |user|
+            column(:first_name)    {|user| user.first_name }
+            column(:last_name)    {|user| user.last_name }
+            column(:email)    {|user| user.email }
+            column(:created_at)    {|user| time_ago_in_words(user.created_at) + " ago" }
+          end
+        end
+      end
+
+      column do
+        panel "Recent Created Shops" do
+          table_for Shop.order('id desc').limit(10).each do |shop|
+            column(:name)    {|shop| shop.name }
+            column(:description)    {|shop| shop.description }
+            column(:created_at)    {|shop| time_ago_in_words(shop.created_at) + " ago" }
+          end
+        end
+      end
+    end
+
+    columns do
+      column do
+        panel "Recent Listed Products" do
+          table_for Product.order('id desc').limit(10).each do |product|
+            column(:name)    {|product| product.name }
+            column(:description)    {|product| product.description }
+            column(:price) {|product| product.price}
+            column(:by_shop) {|product| product.shop.name }
+            column(:created_at)    {|product| time_ago_in_words(product.created_at) + " ago" }
+          end
+        end
+      end
+
+      column do
+        panel "Recent Trades" do
+          table_for Purchase.order('id desc').limit(10).each do |purchase|
+            column(:buyer_email)   {|purchase| purchase.user.email }
+            column(:product) {|purchase| purchase.product.name }
+            column(:by_shop) {|purchase| purchase.product.shop.name }
+            column(:purchase_at)    {|purchase| time_ago_in_words(purchase.created_at) + " ago" }
+          end
+        end
       end
     end
 
