abcf86411d67ea23a18e370b3c049175f3e59e26
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-03-07 17:45:29 +0000

    Add links on items in panels.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -15,6 +15,7 @@ ActiveAdmin.register_page "Dashboard" do
       column do
         panel "Recent Registered Users" do
           table_for User.order('id desc').limit(10).each do |user|
+            column("User ID") {|user| link_to "##{user.id}", admin_user_path(user)}
             column(:first_name)    {|user| user.first_name }
             column(:last_name)    {|user| user.last_name }
             column(:email)    {|user| user.email }
@@ -26,6 +27,7 @@ ActiveAdmin.register_page "Dashboard" do
       column do
         panel "Recent Created Shops" do
           table_for Shop.order('id desc').limit(10).each do |shop|
+            column("Shop ID") {|shop| link_to "##{shop.id}", admin_shop_path(shop)}
             column(:name)    {|shop| shop.name }
             column(:description)    {|shop| shop.description }
             column(:created_at)    {|shop| time_ago_in_words(shop.created_at) + " ago" }
@@ -38,10 +40,11 @@ ActiveAdmin.register_page "Dashboard" do
       column do
         panel "Recent Listed Products" do
           table_for Product.order('id desc').limit(10).each do |product|
+            column("Product ID") {|product| link_to "##{product.id}", admin_product_path(product)}
             column(:name)    {|product| product.name }
             column(:description)    {|product| product.description }
             column(:price) {|product| number_to_currency product.price}
-            column("From Shop") {|product| product.shop.name }
+            column("From Shop") {|product| link_to product.shop.name, admin_shop_path(product.shop) }
             column("Listed at")    {|product| time_ago_in_words(product.created_at) + " ago" }
           end
         end
@@ -50,9 +53,10 @@ ActiveAdmin.register_page "Dashboard" do
       column do
         panel "Recent Trades" do
           table_for Purchase.order('id desc').limit(10).each do |purchase|
-            column(:buyer_email)   {|purchase| purchase.user.email }
-            column(:product) {|purchase| purchase.product.name }
-            column("From Shop") {|purchase| purchase.product.shop.name }
+            column("Purchase ID") {|purchase| link_to "##{purchase.id}", admin_purchase_path(purchase) }
+            column(:buyer_email)   {|purchase| link_to purchase.user.email, admin_user_path(purchase.user) }
+            column(:product) {|purchase| link_to purchase.product.name, admin_product_path(purchase.product) }
+            column("From Shop") {|purchase| link_to purchase.product.shop.name, admin_shop_path(purchase.product.shop) }
             column("Purchased at")    {|purchase| time_ago_in_words(purchase.created_at) + " ago" }
           end
         end
