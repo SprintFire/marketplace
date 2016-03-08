57119ade3f1c100b1541183364dd0144a0e477ad
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-03-08 12:44:12 +0000

    Add Lockable and change the default admin account of admin.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'godmode@sprintfire.com', password: 'iamyourfather', password_confirmation: 'iamyourfather')
