10a140239fa75fcaa692879849e4ad8ba1717f28
Ionatan Sala <sala.jonatan1995@gmail.com>
2016-03-09 11:16:10 +0000

    refactor code for withdrawal controller

--- app/controllers/withdrawals_controller.rb
+++ app/controllers/withdrawals_controller.rb
@@ -1,6 +1,7 @@
 class WithdrawalsController < ApplicationController
+  before_action :set_shop, only: [:new, :create]
+
   def new
-    @shop = current_user.shops.find(params[:shop_id])
     @overall_balance = WithdrawalsHelper.get_shop_overall_balance(@shop)
     @withdrawal_balance = WithdrawalsHelper.get_withdrawal_balance(@shop)
     @current_balance = @overall_balance - @withdrawal_balance
@@ -8,9 +9,8 @@ class WithdrawalsController < ApplicationController
   end
 
   def create
-    shop = current_user.shops.find(params[:withdrawal][:shop_id])
     withdrawal_amount = (params[:withdrawal][:amount].to_i * 100).abs # get absolute value so user can't enter negative numbers
-    current_balance = WithdrawalsHelper.get_current_balance(shop)
+    current_balance = WithdrawalsHelper.get_current_balance(@shop)
 
     if withdrawal_amount > current_balance
       flash[:danger] = "You can't withdraw more than you own"
@@ -18,14 +18,18 @@ class WithdrawalsController < ApplicationController
       flash[:sucess] = "Withdrawal successfull"
 
       # save withdrawal
-      Withdrawal.create(amount: withdrawal_amount, shop: shop)
+      Withdrawal.create(amount: withdrawal_amount, shop: @shop)
     end
 
-    redirect_to new_shop_withdrawal_path(shop)
+    redirect_to new_shop_withdrawal_path(@shop)
   end
 
   private
   def withdrawals_params
     params.require(:withdrawal).permit(:amount, :shop_id)
+  end
+
+  def set_shop
+    @shop = current_user.shops.find(params[:shop_id])
   end
 end
