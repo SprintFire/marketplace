613264aa36616094b8d2e55adc84c2786f2c5376
Levin George <levingeorge41@gmail.com>
2016-03-14 16:45:38 +0000

    Fixed params of product

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,16 +1,4 @@
 ActiveAdmin.register Product do
-  permit_params :title, :content
-
-  show do
-    attributes_table do
-      row :title
-      row :content
-      table_for post.categories.order('title ASC') do
-        column "Categories" do |category|
-          link_to category.title, [ :admin, category ]
-        end
-      end
-    end
-  end
+  permit_params :name, :description, :price, :quantity, :category_id, :shop_id
 
 end
