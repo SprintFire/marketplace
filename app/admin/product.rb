9de850a7de3ff55da531fc0cdf1498e3f97ae126
Levin George <levingeorge41@gmail.com>
2016-03-14 15:56:12 +0000

    Used category id as a foreign key on products table

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Product do
 
+  show do
+    attributes_table do
+      row :title
+      row :content
+      table_for post.categories.order('title ASC') do
+        column "Categories" do |category|
+          link_to category.title, [ :admin, category ]
+        end
+      end
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314123046) do
+ActiveRecord::Schema.define(version: 20160314155244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,7 +145,10 @@ ActiveRecord::Schema.define(version: 20160314123046) do
     t.datetime "updated_at",  null: false
     t.integer  "shop_id"
     t.integer  "quantity"
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "purchases", force: :cascade do |t|
     t.integer  "user_id",                      null: false
@@ -240,4 +243,5 @@ ActiveRecord::Schema.define(version: 20160314123046) do
     t.datetime "updated_at",                 null: false
   end
 
+  add_foreign_key "products", "categories"
 end
