69039bb670d610e239c518ff486fce611a73f54d
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-03-16 18:33:09 +0000

    Now products under one certain category can be viewed.

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,5 +1,6 @@
 class CategoryController < ApplicationController
   def show
     @category = Category.friendly.find(params[:id])
+    @products = Product.where(category_id: @category.id)
   end
 end
