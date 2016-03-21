bb77d9fc754f31d6ff1f835e8600c0aa4cdd7caa
Ionatan Sala <sala.jonatan1995@gmail.com>
2016-03-21 19:27:46 +0000

    make search work better

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,34 +3,30 @@ class SearchController < ApplicationController
 
     if params[:search] && (!params[:search][:lat].blank? || !params[:search][:lng].blank?)
       @shops = Shop.near([params[:search][:lat], params[:search][:lng]], 50)
-      @products = @shops.map { |shop| shop.products }
+      @products = @shops.map{|shop| shop.products}.flatten.uniq
     elsif params[:search] && (params[:search][:lat].blank? || params[:search][:lng].blank?) && (params[:search_category] && params[:search_category].blank?) && (params[:keyword_search] && params[:keyword_search].blank?)
       @shops = Shop.all
       @products = Product.all
-    elsif params[:search] && (params[:search][:lat].blank? || params[:search][:lng].blank?)
-      @shops = []
-      @products = []
     else
       @shops = Shop.all
       @products = Product.all
     end
 
     if params[:search_category] && !params[:search_category].blank?
-      # get all results from category search
       @products_from_category = Category.find(params[:search_category]).products
+      # find all shop with that product category
       @shops_with_category = @products_from_category.map{|product| product.shop }
       # merge similarities and remove duplicates
-      @shops = @shops.concat(@shops_with_category).uniq
-      @products = @products.concat(@products_from_category).uniq
+      @shops = (@shops & @shops_with_category).flatten.uniq
+      @products = (@products & @products_from_category).flatten.uniq
     end
 
     if params[:keyword_search] && !params[:keyword_search].blank?
-      # get all results from keyword search
+      # just keyword entered
       @products_from_keyword = Product.where('UPPER(name) LIKE UPPER(?)', "%#{params[:keyword_search]}%")
       @shops_with_keyword = @products_from_keyword.map{|product| product.shop }
-      # merge similarities
-      @shops = @shops.concat(@shops_with_keyword).uniq
-      @products = @products.concat(@products_from_keyword).uniq
+      @shops = (@shops & @shops_with_keyword).flatten.uniq
+      @products = (@products & @products_from_keyword).flatten.uniq
     end
 
 
