f3faf48823015ab26c19f312015c44d95a7b165e
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-03-24 12:20:14 +0000

    Modify Users Controller Testing.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "GET user#edit" do
+  describe "Users controller" do
+    login_user
+    it "should have a current_user" do
+      # note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
+      expect(subject.current_user).to_not eq(nil)
+    end
+
     it "goes to edit page" do
       get :edit
-      expect(response).to success
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "clicks update button" do
+      get :update
+      expect(response).to be_success
       expect(response).to have_http_status(200)
     end
   end
