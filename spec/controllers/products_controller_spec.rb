ac81af511b97bcf4bbf7c0b2405a01960a60b7ff
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-03-24 18:38:47 +0000

    Add before syntax.

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,59 +2,52 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
 
-  describe "Products controller" do
+  describe "Logged in user" do
     login_user
+    before :each do
+      @shop = create(:shop, user: subject.current_user)
+      p @shop
+    end
+
     it "goes to all product page" do
-      shop = create(:shop, user: subject.current_user)
-      p shop
-      get :index, shop_id: shop.id
+      get :index, shop_id: @shop.id
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "goes to the creating product page" do
-      shop = create(:shop, user: subject.current_user)
-      p shop
-      get :new, shop_id: shop.id
+      get :new, shop_id: @shop.id
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "creates a product" do
-      shop = create(:shop, user: subject.current_user)
-      product = attributes_for(:product, shop: shop)
-      p shop
+      product = attributes_for(:product, shop: @shop)
       p product
-      post :create, { shop_id: shop.id, product: product }
+      post :create, { shop_id: @shop.id, product: product }
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "shows the specified product" do
-      shop = create(:shop, user: subject.current_user)
-      product = create(:product, shop: shop)
-      p shop
+      product = create(:product, shop: @shop)
       p product
-      get :show, { shop_id: shop.id, id: product.id }
+      get :show, { shop_id: @shop.id, id: product.id }
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "goes to the editing product page" do
-      shop = create(:shop, user: subject.current_user)
-      product = create(:product, shop: shop)
-      p shop
+      product = create(:product, shop: @shop)
       p product
-      get :edit, { shop_id: shop.id, id: product.id }
+      get :edit, { shop_id: @shop.id, id: product.id }
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "updates the product information" do
-      shop = create(:shop, user: subject.current_user)
-      product = create(:product, shop: shop)
-      p shop
-      put :update, { shop_id: shop.id, id: product.id, product: product.attributes = { name: 'New name' } }
+      product = create(:product, shop: @shop)
+      put :update, { shop_id: @shop.id, id: product.id, product: product.attributes = { name: 'New name' } }
       product.reload
       p product
       expect(response).to redirect_to shop_product_path
@@ -62,10 +55,8 @@ RSpec.describe ProductsController, type: :controller do
     end
 
     it "deletes the product" do
-      shop = create(:shop, user: subject.current_user)
-      product = create(:product, shop: shop)
-      p shop
-      delete :destroy, { shop_id: shop.id, id: product.id }
+      product = create(:product, shop: @shop)
+      delete :destroy, { shop_id: @shop.id, id: product.id }
       expect(response).to redirect_to dashboard_path
       expect(response).to have_http_status(302)
     end
