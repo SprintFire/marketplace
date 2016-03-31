b67eddfe51f3585797ba113febe43163c7e8bd51
Levin George <levingeorge41@gmail.com>
2016-03-31 12:09:26 +0100

    Shop model testing finished

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe Shop, type: :model do
     it "is invalid without an email_id" do
       expect(build(:shop, email_id: nil)).not_to be_valid
     end
+    # it { should validate_presence_of :picture_size }
   end
 
   describe "Length Test" do
@@ -22,7 +23,24 @@ RSpec.describe Shop, type: :model do
       should validate_length_of(:name).is_at_least(2).on(:create)
     end
     it "description attribute" do
-      should validate_length_of(:description).is_at_least(10)
+      should validate_length_of(:description).is_at_least(10).on(:create)
     end
   end
+
+  describe "Association Test" do
+    it { should belong_to(:user) }
+    it { should have_many(:withdrawals) }
+    it { should have_many(:products).dependent(:delete_all) }
+
+    # it "deletes dependent products" do
+    #   shop = build(:shop)
+    #   product = build(:product)
+    #
+    #   shop.products << product
+    #   shop.delete
+    #
+    #   expect { shop.destroy}.to change {Product.count}
+    # end
+  end
+
 end
