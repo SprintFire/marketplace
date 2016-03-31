a6965a01d7305e36c94079c43b4c802c6108cc2d
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-03-31 15:35:01 +0100

    Add basic APIs for shops and products.

--- app/api/api.rb
+++ app/api/api.rb
@@ -8,13 +8,47 @@ class API < Grape::API
       User.all
     end
 
-    desc "return a user information"
+    desc "return the information of a user"
     params do
         requires :id, :type => Integer, :desc => "user id"
     end
     route_param :id do
         get do
             User.find(params[:id])
+        end
+    end
+  end
+
+  resource :shops do
+    desc "List all shops"
+    get do
+      Shop.all
+    end
+
+    desc "return the information of a shop "
+    params do
+        requires :id, :type => Integer, :desc => "shop id"
+    end
+    route_param :id do
+        get do
+            Shop.find(params[:id])
+        end
+    end
+  end
+
+  resource :products do
+    desc "List all products"
+    get do
+      Product.all
+    end
+
+    desc "return the information of a product"
+    params do
+        requires :id, :type => Integer, :desc => "product id"
+    end
+    route_param :id do
+        get do
+            Product.find(params[:id])
         end
     end
   end
