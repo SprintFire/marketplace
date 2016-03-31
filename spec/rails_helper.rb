48bfc33d30ad576e4e08f6fbd9490c008f0fb138
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-03-31 12:26:09 +0100

    Add Shoulda Matchers.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,20 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+    # Choose one or more libraries:
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+    # Or, choose the following (which implies all of the above):
+    # with.library :rails
+  end
+end
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
