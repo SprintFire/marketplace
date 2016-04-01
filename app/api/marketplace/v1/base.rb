26e8c118194cd14f2a014dc12f79ac5cb9329aa2
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-04-01 13:22:12 +0100

    Add exception handle.

--- app/api/marketplace/v1/base.rb
+++ app/api/marketplace/v1/base.rb
@@ -3,6 +3,10 @@ module Marketplace
     class Base < Grape::API
       version 'v1', using: :path
 
+      rescue_from ActiveRecord::RecordNotFound do |e|
+        error!({ error_code: 404, error_messages: e.message }, 404)
+      end
+
       mount Marketplace::V1::Users
       mount Marketplace::V1::Shops
       mount Marketplace::V1::Products
