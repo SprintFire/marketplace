109c3d5a1b98aa97bab8b6f16b704777068100e9
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-04-01 17:02:26 +0100

    Remove unnecessary authentication code.

--- app/api/marketplace/api.rb
+++ app/api/marketplace/api.rb
@@ -13,20 +13,5 @@ class Marketplace::API < Grape::API
     error!({ error_code: 404, error_messages: e.message }, 404)
   end
 
-  helpers do
-    def current_user
-      user = User.where(authentication_token: params[:auth_token], is_approved: true).first
-      if user
-        @current_user = user
-      else
-        false
-      end
-    end
-
-    def authenticate!
-      error!('401 Unauthorized', 401) unless current_user
-    end
-  end
-
   mount Marketplace::V1::Base
 end
