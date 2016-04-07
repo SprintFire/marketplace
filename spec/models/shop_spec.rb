c0a698ba5e98c8cfa7b0161a8187d2b20f74cba3
Levin George <levingeorge41@gmail.com>
2016-04-07 11:40:03 +0100

    Removed the comments

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -15,7 +15,6 @@ RSpec.describe Shop, type: :model do
     it "is invalid without an email_id" do
       expect(build(:shop, email_id: nil)).not_to be_valid
     end
-    # it { should validate_presence_of :picture_size }
   end
 
   describe "Length Test" do
@@ -31,16 +30,6 @@ RSpec.describe Shop, type: :model do
     it { should belong_to(:user) }
     it { should have_many(:withdrawals) }
     it { should have_many(:products).dependent(:delete_all) }
-
-    # it "deletes dependent products" do
-    #   shop = build(:shop)
-    #   product = build(:product)
-    #
-    #   shop.products << product
-    #   shop.delete
-    #
-    #   expect { shop.destroy}.to change {Product.count}
-    # end
   end
 
 end
