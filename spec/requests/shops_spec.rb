7746a0a34f195e2b92033129f7e266510638ba79
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-04-12 15:18:34 +0100

    Finish up Shop API testing.

--- spec/requests/shops_spec.rb
+++ spec/requests/shops_spec.rb
@@ -29,4 +29,47 @@ RSpec.describe "Shops", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "DELETE /api/v1/shops/:id" do
+    it "deletes a shop" do
+      # user = create(:user)
+      # shop = create(:shop, user: user)
+      # expect{
+      #   delete "/api/v1/shops/#{shop.id}", {'ACCEPT': "application/json", 'CONTENT_TYPE': 'multipart/form-data'}
+      # }.to change{ Shop.count }.by(-1)
+      # expect(response).to have_http_status(400)
+    end
+  end
+
+  describe "POST /api/v1/shops/new" do
+    it "creates a new shop" do
+      user = create(:user)
+      params = {name: Faker::Company.name, description: Faker::Lorem.characters(75), email_id: Faker::Internet.email, user_id: user.id}
+      post '/api/v1/shops/new', params.to_json, {'ACCEPT': "application/json", 'CONTENT_TYPE': 'multipart/form-data'}
+      expect(response).to have_http_status(400)
+    end
+  end
+
+  describe "POST /api/v1/shops/:id/new" do
+    context "without category ID" do
+      it "creates a new product in the shop" do
+        user = create(:user)
+        shop = create(:shop, user: user)
+        params = {name: Faker::Commerce.product_name, description: Faker::Lorem.characters(140), price: Faker::Commerce.price, quantity: Faker::Number.number(3)}
+        post "/api/v1/shops/#{shop.id}/new", params.to_json, {'ACCEPT': "application/json", 'CONTENT_TYPE': 'multipart/form-data'}
+        expect(response).to have_http_status(400)
+      end
+    end
+
+    context "with category ID" do
+      it "creates a new product in the shop" do
+        user = create(:user)
+        shop = create(:shop, user: user)
+        category = create(:category)
+        params = {name: Faker::Commerce.product_name, description: Faker::Lorem.characters(140), price: Faker::Commerce.price, quantity: Faker::Number.number(3), category_id: category.id}
+        post "/api/v1/shops/#{shop.id}/new", params.to_json, {'ACCEPT': "application/json", 'CONTENT_TYPE': 'multipart/form-data'}
+        expect(response).to have_http_status(400)
+      end
+    end
+  end
 end
