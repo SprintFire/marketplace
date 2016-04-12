2134dae12e5d0382e82aeedb3ea2dbae13f8b5d1
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-04-12 14:32:19 +0100

    Finish up the testing for Users API.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -25,4 +25,37 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(400)
     end
   end
+
+  describe "GET /api/v1/users/:id" do
+    it 'gets information of a user' do
+      user = create(:user)
+      get "/api/v1/users/#{user.id}"
+      expect(response.body).to eq user.to_json
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /api/v1/users/:id/cards" do
+    it 'gets all cards of a user' do
+      user = create(:user)
+      get "/api/v1/users/#{user.id}/cards"
+      expect(response.body).to eq Card.where(user_id: user.id).all.to_json
+      expect(JSON.parse(response.body)).to be_an_instance_of(Array)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /api/v1/users/:id/purchases" do
+    it 'gets all purchases by a user' do
+      user = create(:user)
+      get "/api/v1/users/#{user.id}/purchases"
+      expect(response.body).to eq Purchase.where(user_id: user.id).all.to_json
+      expect(JSON.parse(response.body)).to be_an_instance_of(Array)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /api/v1/users/:id/purchases" do
+    
+  end
 end
