030ccb151f534ae71c0dc835fe1ea1ad876968f7
Zhenbang “Stark” Xiao <xiaozhenbang@gmail.com>
2016-04-12 14:12:19 +0100

    Add register user testing.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe "Users", type: :request do
       expect(response.body).to eq User.all.to_json
     end
   end
+
+  describe "POST /api/v1/users/auth/sign_up" do
+    it 'registers a user' do
+      params = {email: "#{Faker::Internet.email}", first_name: "#{Faker::Name.first_name}", last_name: "#{Faker::Name.last_name}", password: "#{Faker::Internet.password(8)}"}
+      post '/api/v1/users/auth/sign_up', params.to_json, {'ACCEPT': "application/json", 'CONTENT_TYPE': 'multipart/form-data'}
+      expect(response).to have_http_status(400)
+    end
+  end
 end
