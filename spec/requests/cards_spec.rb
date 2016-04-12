ff6c019fdce96a5de21929340b0075c4334af2ce
Levin George <levingeorge41@gmail.com>
2016-04-12 14:40:43 +0100

    added an extra test

--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe "Cards", type: :request do
       get '/api/v1/cards'
       expect(JSON.parse(response.body)).to be_an_instance_of(Array)
       expect(response.body).to eq Card.all.to_json
+      expect(response).to have_http_status(200)
     end
 
     it "GET /api/v1/cards/:id" do
       card = create(:card)
       get "/api/v1/cards/#{card.id}"
       expect(response.body).to eq card.to_json
+      expect(response).to have_http_status(200)
     end
   end
 end
